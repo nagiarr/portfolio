57eb19919d074ef4287c5971f937941cfb7d0b01
nagiarr <nagiarr@yahoo.co.jp>
2025-02-14 16:33:25 +0900

    Resize QRCode

--- JS/portfolio.js
+++ JS/portfolio.js
@@ -1,34 +1,34 @@
 const canvas = document.getElementById('backgroundCanvas');
 const ctx = canvas.getContext('2d');
 
-// サイズを設定
+    // サイズを設定
     function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
     }
     resizeCanvas();
     window.addEventListener('resize', resizeCanvas);
 
-// アニメーション用の円の設定
+    // アニメーション用の円の設定
     const circles = [];
     const circleCount = 50;
 
     for (let i = 0; i < circleCount; i++) {
-        circles.push({
-            x: Math.random() * canvas.width,
-            y: Math.random() * canvas.height,
-            radius: Math.random() * 5 + 2,
-            xSpeed: Math.random() * 2 - 1,
-            ySpeed: Math.random() * 2 - 1,
-            color: 'rgba(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', 0.8)'
-        });
+      circles.push({
+        x: Math.random() * canvas.width,
+        y: Math.random() * canvas.height,
+        radius: Math.random() * 5 + 2,
+        xSpeed: Math.random() * 2 - 1,
+        ySpeed: Math.random() * 2 - 1,
+        color: 'rgba(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', 0.8)'
+      });
     }
 
 // 描画関数
     function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    circles.forEach(circle => {
+      circles.forEach(circle => {
         // 円を描画
         ctx.beginPath();
         ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2);
@@ -42,9 +42,9 @@ const ctx = canvas.getContext('2d');
         // 境界を超えた場合の処理
         if (circle.x < 0 || circle.x > canvas.width) circle.xSpeed *= -1;
         if (circle.y < 0 || circle.y > canvas.height) circle.ySpeed *= -1;
-    });
+      });
 
-    requestAnimationFrame(draw);
+      requestAnimationFrame(draw);
     }
 
 // アニメーション開始
