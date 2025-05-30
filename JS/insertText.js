563561376bdfb301a26553e012ad2fcfa8cdef45
nagiarr <nagiarr@yahoo.co.jp>
2025-05-30 20:18:06 +0900

    add workExperience
    CSSにてテーブルの背景を色付きにする
    テーブルの背景色を交互に変化
    
    職務経歴の内容を修正

--- JS/insertText.js
+++ JS/insertText.js
@@ -1,2 +1,8 @@
 let p = document.querySelector('.greet');
   p.after('ここは柳町有俊のHPです。');
+
+const table = document.getElementsByClassName('table');
+for(let i = 0; i < table.length; i++){
+  const table_row = table[i];
+  table_row.computedStyleMap.bgcolor = black;
+}
