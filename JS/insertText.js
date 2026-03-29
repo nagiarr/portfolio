d8015734c1d95b25d6c04cbe3702bf693c80587c
nagiarr <nagiarr@yahoo.co.jp>
2026-03-29 18:58:05 +0900

    基本情報の詳細データをjsonから読み込ませる方式に変更

--- JS/insertText.js
+++ JS/insertText.js
@@ -1,8 +1,26 @@
-let p = document.querySelector('.greet');
-  p.after('ここは柳町有俊のHPです。');
+document.addEventListener('DOMContentLoaded', () => {
 
-const table = document.getElementsByClassName('table');
-for(let i = 0; i < table.length; i++){
-  const table_row = table[i];
-  table_row.computedStyleMap.bgcolor = black;
-}
+    fetch('./data/profile.json')
+      .then(response => response.json())
+      .then(data => {
+        const name = document.getElementById('name');
+        name.textContent = data.profile_data.name + "(" + data.profile_data.yomi + ")";
+        
+        const date_of_birth = document.getElementById('date_of_birth');
+        date_of_birth.textContent = data.profile_data.date_of_birth;
+
+        const address = document.getElementById('address');
+        address.textContent = data.profile_data.address;
+
+        const site_of_bith = document.getElementById('site_of_birth');
+        site_of_bith.textContent = data.profile_data.site_of_birth;
+
+        const refresh = document.getElementById('refresh');
+        refresh.textContent = data.profile_data.refresh;
+
+      })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+
+});
